2bf984420c8d70904d4a09c262c1292953f106f9
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-09 10:36:58 -0400

    11-size

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,30 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
+	if (tree != NULL)
 	{
 		size_t left = 0, right = 0;
 
-		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((left > right) ? left : right);
+		if (tree->left != NULL)
+		{
+			left = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			right = 1 + binary_tree_height(tree->right);
+		}
+		if (left > right)
+		{
+			return (left);
+		}
+		else
+		{
+			return (right);
+		}
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
 
