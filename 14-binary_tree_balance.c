09e3c3235f19e8e811c532c4a5d192753fbce4ac
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-09 22:08:50 -0400

    task 14 fix, learned about ternary operators

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,9 +7,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree != NULL)
 	{
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+
+		return (left_height - right_height);
 	}
 	return (0);
 }
@@ -24,24 +30,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		size_t left = 0, right = 0;
-
-		if (tree->left != NULL)
-		{
-			left = 1 + binary_tree_height(tree->left);
-		}
-		if (tree->right != NULL)
-		{
-			right = 1 + binary_tree_height(tree->right);
-		}
-		if (left > right)
-		{
-			return (left);
-		}
-		else
-		{
-			return (right);
-		}
+		size_t left = binary_tree_height(tree->left);
+		size_t right = binary_tree_height(tree->right);
+		
+		return (1 + (left > right ? left : right));
 	}
 	else
 	{
