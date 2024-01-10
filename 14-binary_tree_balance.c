9427d9f2069429138fb1bcd79ff2eba0415d61e4
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-01-09 22:09:44 -0400

    task 14 betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t left = binary_tree_height(tree->left);
 		size_t right = binary_tree_height(tree->right);
-		
+
 		return (1 + (left > right ? left : right));
 	}
 	else
